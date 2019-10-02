a4f3711dd7ca254199c14db9fd5fedddf2483b81
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2019-10-02 20:40:44 -0300

    Structure + Environment

--- shelfProject/src/app/app-routing.module.ts
+++ shelfProject/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+import { LoginComponent } from "./modules/auth/components/login/login.component";
+import { RegistrationComponent } from "./modules/auth/components/registration/registration.component";
+// import { AuthGuard } from "./shared/guards/auth-guard.service";
+
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "cadastro", component: RegistrationComponent },
+  { path: "", redirectTo: "login", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- shelfProject/src/app/app.module.ts
+++ shelfProject/src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from "@angular/common/http";
+import ptBr from '@angular/common/locales/pt';
+import { LOCALE_ID, NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule } from "@angular/router";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AuthModule } from "./modules/auth/auth.module";
+import { MaterialModule } from "./modules/material/material.module";
+import { ToastrModule } from 'ngx-toastr';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+registerLocaleData(ptBr)
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    AuthModule,
+    RouterModule,
     BrowserModule,
-    AppRoutingModule
+    MaterialModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [
+    { provide: LOCALE_ID, useValue: "pt_BR" },
+  ]
 })
-export class AppModule { }
+export class AppModule {}
